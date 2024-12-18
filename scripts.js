19dd5e653be4477288711fae1647265aa009f7a0
Filipe Mendes <fmatiasmendes@gmail.com>
2024-12-18 15:49:17 +0000

    working but with an error on invalid city name

--- scripts.js
+++ scripts.js
@@ -44,11 +44,64 @@ async function getWeatherData(city){
 }
 
 function displayWeatherInfo(data){
+	
+	//erase
 	console.log(data);
+
+	const {
+		name: city,
+		main:{temp, humidity},
+		weather: [{description, id, icon}]
+	} = data;
+
+	card.textContent = "";
+	card.style.display = "flex";
+
+	const cityDisplay = document.createElement("h1");
+	const tempDisplay = document.createElement("p");
+	const humidityDisplay = document.createElement("p");
+	const descDisplay = document.createElement("p");
+	const weatherEmoji = document.createElement("p");
+
+	cityDisplay.textContent = city;
+	tempDisplay.textContent = `${temp}° C`;
+	humidityDisplay.textContent = `Humidity: ${humidity}%`;
+	descDisplay.textContent = description;
+	weatherEmoji.textContent = getWeatherEmoji(id);
+
+	cityDisplay.classList.add("cityDisplay");
+	tempDisplay.classList.add("tempDisplay");
+	humidityDisplay.classList.add("humidityDisplay");
+	descDisplay.classList.add("descDisplay");
+	weatherEmoji.classList.add("weatherEmoji");
+
+	card.appendChild(cityDisplay);
+	card.appendChild(tempDisplay);
+	card.appendChild(humidityDisplay);
+	card.appendChild(descDisplay);
+	card.appendChild(weatherEmoji);
 }
 
 function getWeatherEmoji(weatherId){
 
+	switch(true){
+		case (weatherId >= 200 && weatherId < 300):
+			return "⛈️";
+		case (weatherId >= 300 && weatherId < 400):
+			return "🌧️";
+		case (weatherId >= 500 && weatherId < 600):
+			return "🌦️";
+		case (weatherId >= 600 && weatherId < 700):
+			return "❄️";
+		case (weatherId >= 700 && weatherId < 800):
+			return "🌫️";
+		case (weatherId === 800):
+			return "☀️";
+		case (weatherId > 800):
+			return "☁️";
+		default:
+			return "❓";
+	}
 }
 
 function displayError(message){
