3c4e3f59c8c482e041ec01d103dc8c2392dd7a4a
Filipe Mendes <fmatiasmendes@gmail.com>
2024-12-18 12:19:22 +0000

    function weatherForm.addEventListener done

--- scripts.js
+++ scripts.js
@@ -5,18 +5,24 @@ const card = document.querySelector(".card");
 const apiKey = "89fe191d4ad03c4d24b3fbfd05ea997f";
 
 //functions
-weatherForm.addEventListener("submit", event => {
-	console.log("test1");
+weatherForm.addEventListener("submit", async event => {
+
 	event.preventDefault();
 
 	const city = cityInput.value;
 
 	if(city){
-
+		try{
+			const weatherData = await getWeatherData(city);
+			displayWeatherInfo(weatherData);
+		}
+		catch(error){
+			console.error(error);
+			displayError(error);
+		}
 	}
 	else{
 		displayError("Please enter a city");
-		console.log("test2");
 	}
 });
 
